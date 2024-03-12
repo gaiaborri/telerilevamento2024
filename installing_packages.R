346950be4a59b1f43d1279c54a22ae0337e72f5d
Gaia Borri <162638591+gaiaborri@users.noreply.github.com>
2024-03-12 17:31:42 +0100

    Update installing_packages.R

--- installing_packages.R
+++ installing_packages.R
@@ -1,1 +1,14 @@
+# Installing new packages in R
 
+# install.packages("terra")
+library(terra)
+install.packages("devtools")
+library(devtools)
+
+# install the imageRy package from GitHub
+devtools::install_github("ducciorocchini/imageRy")
+#la funzione dopo i due punti deriva da devtools
+#la funzione install_github mi permette su R di andare a cercare quello che metto 
+tra parentesi su github, quindi "esco" da R
+
+library(imageRy)
