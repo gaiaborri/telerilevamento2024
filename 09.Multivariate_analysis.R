8a8b3de7a9b6c5f31b0e3d7c2c36618be4e4697c
Gaia Borri <162638591+gaiaborri@users.noreply.github.com>
2024-06-07 15:02:29 -0300

    Update 09.Multivariate_analysis.R

--- 09.Multivariate_analysis.R
+++ 09.Multivariate_analysis.R
@@ -15,11 +15,12 @@ b4 <- im.import("sentinel.dolomites.b4.tif") # red
 b8 <- im.import("sentinel.dolomites.b8.tif") # nir
 
 sentdo <- c(b2, b3, b4, b8)
-
-im.plotRGB(sentdo, r=4, g=3, b=2)
-im.plotRGB(sentdo, r=3, g=4, b=2)
+#Creo il plot:
+im.plotRGB(sentdo, r=4, g=3, b=2) #banda 4 montata sul rosso, banda 3 sul verde e banda 2 sul blu
+#oppure:
+im.plotRGB(sentdo, r=3, g=4, b=2)# banda 3 montata sul rosso, banda 4 sul verde e banda 2 sul blu
 
 #La funzione pairs() in R è utilizzata per creare una matrice di scatterplot che mostra le relazioni tra 
 #tutte le coppie di variabili in un set di dati. È particolarmente utile quando si desidera esaminare le 
-#correlazioni e le distribuzioni tra più variabili contemporaneamente.
+#correlazioni e le distribuzioni tra più variabili contemporaneamente. Si basa sulla matrice di Pearson
 pairs(sentdo)
